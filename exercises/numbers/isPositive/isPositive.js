a50ad5b3bc62043de4327469dc032eb8a6ee8454
kross00 <59896486+kross00@users.noreply.github.com>
2020-01-15 11:52:25 -0800

    Update isPositive.js

--- exercises/numbers/isPositive/isPositive.js
+++ exercises/numbers/isPositive/isPositive.js
@@ -4,17 +4,13 @@
  * @param {number} num The input number
  * @returns {boolean} True if num is positive and false otherwise
  */
-function isPositive(num) {
-  return _____;
+function isPositive(num){
+if(num > 0) {
+  return true;
+}else{
+return false }
 }
 
-if (require.main === module) {
-  console.log('Running sanity checks for isPositive:');
 
-  console.log(isPositive(0) === false);
-  console.log(isPositive(1) === true);
-  console.log(isPositive(-1) === false);
-  console.log(isPositive(-100.20) === false);
-}
-
-module.exports = isPositive;
+console.log(isPositive(22));
+console.log(isPositive(-22));
