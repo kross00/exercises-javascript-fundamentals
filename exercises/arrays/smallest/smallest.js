85e14027a05e55744ce586f0812638385eff4f63
kross00 <59896486+kross00@users.noreply.github.com>
2020-01-20 01:44:40 -0800

    Update smallest.js

--- exercises/arrays/smallest/smallest.js
+++ exercises/arrays/smallest/smallest.js
@@ -7,28 +7,22 @@
  * @returns {number} The smallest element in the array
  */
 function smallest(array) {
-  let _____ = _____;
+  let smallestSoFar = array[0];
 
-  for (let _____ of _____) {
-    if (_____) {
-      _____ = _____;
+  for (let element of array) {
+    if (element < smallestSoFar) {
+      smallestSoFar = element;
     }
   }
 
-  return _____;
+  return smallestSoFar;
 }
 
 if (require.main === module) {
   console.log('Running sanity checks for smallest:');
 
-  console.log(smallest([1, 2, 3]) === 1);
-  console.log(smallest([0, -100, 50, -200]) === -200);
-  console.log(smallest([-200, -400, -100, -300]) === -400);
-  console.log(smallest([0]) === 0);
-  console.log(smallest([1]) === 1);
-  console.log(smallest([-1]) === -1);
-  console.log(smallest([11, 11, 11]) === 11);
-  console.log(smallest([-22, -11, -22]) === -22);
+  console.log(smallest([1, 2, 3]));
+
 }
 
 module.exports = smallest;
