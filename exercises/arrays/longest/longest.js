8feaf136bb63878a5106efe7a382ea672ed267d8
kross00 <59896486+kross00@users.noreply.github.com>
2020-01-27 15:22:41 -0800

    Update longest.js

--- exercises/arrays/longest/longest.js
+++ exercises/arrays/longest/longest.js
@@ -7,14 +7,24 @@
  * @returns {number} The longest string in the array
  */
 function longest(array) {
-  // This is your job. :)
-}
+    let longestSoFar = array[0];
+    for(let element of array){
+        if (element.length > longestSoFar.length){
+            longestSoFar = element 
+        }
+    }
+    return longestSoFar 
+  }
 
-if (require.main === module) {
-  console.log('Running sanity checks for longest:');
 
-  // Add your own sanity checks here.
-  // How else will you be sure your code does what you think it does?
-}
+  if (require.main === module) {
+    console.log('Running sanity checks for longest:');
+  
+    console.log(longest(['Kendahl','Ryan', 'Ross']));
 
-module.exports = longest;
+console.log(longest(['School', 'Pineapple', 'Bow'])); 
+
+   
+  }
+  
+  module.exports = longest;
